d165d6ea062f6ba8f65abd5a983fc0feac4b6745
Zeshan Ayub <unofficialmz@gmail.com>
2023-11-14 18:07:42 +0500

    Added Project files

---
+++ chatbot/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+from bardapi import Bard
+# Create your views here.
+def chatbot(request):
+    if request.method == 'POST':
+        message = request.POST.get('message')
+        response = "Thanks for choosing YD chatbot!"
+        bard = Bard(token='xxxxx') # Your token here
+        response = bard.get_answer(message)['content']
+        return JsonResponse({'message':message,'response': response})
+    return render(request, 'chatbot.html')
